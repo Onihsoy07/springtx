891767f9d6aaa605d07d7f7b8af71685fd46746e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-07 22:31:20 +0900

    트랜잭션 전파 외부 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -102,5 +102,23 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋 완료");
     }
 
+    @Test
+    void outerRollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = tm.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.insNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = tm.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.insNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋 시작");
+        tm.commit(inner);
+        log.info("내부 트랜잭션 커밋 완료");
+
+        log.info("외부 트랜잭션 롤백 시작");
+        tm.rollback(outer);
+        log.info("외부 트랜잭션 롤백 완료");
+    }
 
 }
