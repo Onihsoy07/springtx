fe7bcbd2a66f3f99e27efd9aa6c3c5ee874bcedb
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-09 23:07:41 +0900

    내부 트랜잭션 분리

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,7 +1,6 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -142,6 +142,28 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋 시작");
         assertThatThrownBy(() -> tm.commit(outer))
                 .isInstanceOf(UnexpectedRollbackException.class);
+        log.info("외부 트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void innerRollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = tm.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.insNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute defaultTransactionAttribute = new DefaultTransactionAttribute();
+        //새로운 트랜잭션 생성 세팅
+        defaultTransactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = tm.getTransaction(defaultTransactionAttribute);
+        log.info("inner.insNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백 시작");
+        tm.rollback(inner);
+        log.info("내부 트랜잭션 롤백 완료");
+
+        log.info("외부 트랜잭션 커밋 시작");
+        tm.commit(outer);
         log.info("외부 트랜잭션 커밋 완료");
     }
 
