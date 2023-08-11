394a16f057e99655b462949e4a4b0e15f35fd671
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-11 22:51:40 +0900

    트랜잭션 전파 활용 로그 에러로 로그만 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -30,5 +31,20 @@ class MemberServiceTest {
         //then
         assertTrue(memberRepository.findById(username).isPresent());
         assertTrue(logRepository.findById(username).isPresent());
+    }
+
+    //member 저장, log 에러로 롤백(문제 됨)
+    @Test
+    void outerTxOFF_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.findById(username).isPresent());
+        assertTrue(logRepository.findById(username).isEmpty());
     }
 }
