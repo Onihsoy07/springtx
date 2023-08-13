f1e76b33dd53c0e8f1818147fc257cd11eff50e6
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-13 23:42:18 +0900

    논리 트랜잭션 에러 catch 시 물리 트랜잭션 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -110,6 +111,28 @@ class MemberServiceTest {
         //when
         assertThatThrownBy(() -> memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.findById(username).isEmpty());
+        assertTrue(logRepository.findById(username).isEmpty());
+    }
+
+    /**
+     * memberService       @Transaction : ON
+     * memberRepository    @Transaction : ON
+     * logRepository       @Transaction : ON(Exception)
+     *
+     * memberService.joinV2 -> exception catch로 잡음
+     * 논리 트랜잭션(LogRepository)에서 rollbackOnly=true를 하기 때문에 에러 잡아도 롤백 됨
+     */
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "로그예외-recoverException_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class); //내부 트랜잭션이 롤백 되었는데 외부 트랜잭션 커밋하면 UnexpectedRollbackException 발생
 
         //then
         assertTrue(memberRepository.findById(username).isEmpty());
